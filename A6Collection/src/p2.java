22cb4074de12377cfbee62b8f506ef419286abcc
shrvaidy <shrvaidy@LIN25007660.corp.capgemini.com>
2021-11-05 10:18:30 +0530

    collection assignments

--- A6Collection/src/p2.java
+++ A6Collection/src/p2.java
@@ -1,11 +1,9 @@
 import java.util.HashSet;
 
-
-public class Problem2 {
+public class p2 {
 
 	public static void main(String [] args)
-	{
-		
+	{	
 	HashSet<Integer> myhashset= new HashSet<>();
 	myhashset.add(9);
 	myhashset.add(18);
@@ -17,12 +15,7 @@ public class Problem2 {
 	myhashset.add(72);
 	myhashset.add(81);
 	myhashset.add(90);
-	 myhashset.add(9);
-	System.out.println(myhashset);
-	
-	
-	
+	myhashset.add(9);
+	System.out.println(myhashset);	
 	}
-	
-	
 }
