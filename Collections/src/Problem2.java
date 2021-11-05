535b723abecce5db7ed2c318e5dcd56718b25ee0
shrvaidy <shrvaidy@LIN25007660.corp.capgemini.com>
2021-11-05 09:50:08 +0530

    collection assignment

--- Collections/src/Problem2.java
+++ Collections/src/Problem2.java
@@ -1,22 +1,28 @@
 import java.util.HashSet;
 
+
 public class Problem2 {
 
-	public static void main(String[] args) {
-		HashSet<Integer> hash = new HashSet<>();
-		hash.add(9);
-		hash.add(18);
-		hash.add(27);
-		hash.add(36);
-		hash.add(45);
-		hash.add(54);
-		hash.add(63);
-		hash.add(72);
-		hash.add(81);
-		hash.add(90);
-		hash.add(9);
-		System.out.println(hash);
-
+	public static void main(String [] args)
+	{
+		
+	HashSet<Integer> myhashset= new HashSet<>();
+	myhashset.add(9);
+	myhashset.add(18);
+	myhashset.add(27);
+	myhashset.add(36);
+	myhashset.add(45);
+	myhashset.add(54);
+	myhashset.add(63);
+	myhashset.add(72);
+	myhashset.add(81);
+	myhashset.add(90);
+	 myhashset.add(9);
+	System.out.println(myhashset);
+	
+	
+	
 	}
-
+	
+	
 }
